8e19028f1debed61193e79467612ab37c5ebe220
Sebastian <sebastian.borsch1996@gmail.com>
2026-05-17 21:34:06 +0200

    Redesigned screen layout

--- crates/tui/src/app.rs
+++ crates/tui/src/app.rs
@@ -4,7 +4,11 @@ use crate::screens::{ScreenType, Screens};
 use crate::TuiBackend;
 use crossterm::event::Event;
 use ratatui::backend::CrosstermBackend;
-use ratatui::{Frame, Terminal};
+use ratatui::layout::HorizontalAlignment::Center;
+use ratatui::layout::{Alignment, Constraint, Layout};
+use ratatui::prelude::Line;
+use ratatui::widgets::{Block, BorderType, Borders, Paragraph};
+use ratatui::{symbols, Frame, Terminal};
 use std::io::Stdout;
 
 pub struct App {
@@ -63,9 +67,42 @@ impl App {
 
     fn render(&mut self, frame: &mut Frame) {
         let area = frame.area();
+        let main_block = Block::default()
+            .borders(Borders::ALL)
+            .border_type(BorderType::Thick)
+            .title(" Chrono Forge ")
+            .title_alignment(Center);
+        frame.render_widget(main_block, area);
 
+        let mut screen_rect = area;
+        screen_rect.width -= 2;
+        screen_rect.height -= 2;
+        screen_rect.x += 1;
+        screen_rect.y += 1;
+
+        let app_layout_rects = Layout::vertical([
+            Constraint::Length(3), // tab view
+            Constraint::Length(1), // separator
+            Constraint::Min(1),    // screen
+            Constraint::Length(1), // separator
+            Constraint::Length(3), // help bar
+        ])
+        .split(screen_rect);
+
+        let separator =
+            symbols::line::HORIZONTAL.repeat(app_layout_rects[1].width.saturating_sub(2) as usize);
+        let separator_widget = Paragraph::new(Line::from(separator));
+        let mut rect = app_layout_rects[1];
+        rect.x = rect.x + 1;
+        frame.render_widget(separator_widget, rect);
+
+        let mut help_text = String::new();
+        let screen_area = app_layout_rects[2];
         match self.current_screen {
-            ScreenType::ProjectOverview => self.screens.project_overview.render(frame, area),
+            ScreenType::ProjectOverview => {
+                self.screens.project_overview.render(frame, screen_area);
+                help_text = self.screens.project_overview.get_help_text();
+            }
             ScreenType::Timer => {
                 todo!()
             }
@@ -73,6 +110,19 @@ impl App {
                 todo!()
             }
         }
+
+        let separator =
+            symbols::line::HORIZONTAL.repeat(app_layout_rects[3].width.saturating_sub(2) as usize);
+        let separator_widget = Paragraph::new(Line::from(separator));
+        let mut rect = app_layout_rects[3];
+        rect.x = rect.x + 1;
+        frame.render_widget(separator_widget, rect);
+
+        // help box
+        let help_box = Paragraph::new(Line::from(help_text).alignment(Alignment::Center));
+        let mut rect = app_layout_rects[4];
+        rect.y += 1;
+        frame.render_widget(help_box, rect);
     }
 
     fn handle_event(&mut self, event: Event) -> Option<AppAction> {
--- crates/tui/src/screens/project_overview/project_overview.rs
+++ crates/tui/src/screens/project_overview/project_overview.rs
@@ -6,29 +6,40 @@ use crate::widgets::selectable_list::SelectableList;
 use crate::widgets::text_edit_dialog::{DialogResult, TextEditDialog};
 use crossterm::event::{Event, KeyCode, KeyEvent};
 use domain::types::Project;
-use ratatui::layout::{Constraint, Layout, Rect};
-use ratatui::widgets::{Block, Borders};
+use ratatui::layout::{Alignment, Constraint, Layout, Rect};
+use ratatui::prelude::Line;
+use ratatui::widgets::{Block, Borders, Paragraph};
 use ratatui::Frame;
 
 pub struct ProjectOverviewScreen {
     view_model: ProjectOverviewViewModel,
     mode: Mode,
     projects_list_widget: SelectableList,
+    task_list_widget: SelectableList,
     text_edit_dialog: Option<TextEditDialog>,
     confirmation_dialog: Option<ConfirmationDialog>,
     selected_project: Option<Project>,
+    help_text: String,
 }
 
 impl ProjectOverviewScreen {
     pub fn new() -> Self {
-        Self {
+       let mut this =  Self {
             view_model: ProjectOverviewViewModel::default(),
             mode: Mode::ProjectSelection,
             projects_list_widget: SelectableList::default(),
+            task_list_widget: SelectableList::default(),
             text_edit_dialog: None,
             confirmation_dialog: None,
             selected_project: None,
-        }
+            help_text: String::new(),
+        };
+        this.enable_project_selection_mode();
+        this
+    }
+
+    pub fn get_help_text(&self) -> String {
+        self.help_text.clone()
     }
 
     pub fn set_view_model(&mut self, view_model: ProjectOverviewViewModel) {
@@ -41,23 +52,25 @@ impl ProjectOverviewScreen {
             .map(|p| p.project.name.clone())
             .collect();
         self.projects_list_widget.items = project_names;
+        self.projects_list_widget.title = Some("Projects".to_string());
     }
 
     pub fn render(&mut self, frame: &mut Frame, area: Rect) {
-        let chunks = Layout::vertical([
-            Constraint::Length(3),
-            Constraint::Min(1),
-            Constraint::Length(3),
-        ])
-        .split(area);
 
-        // Header
-        let header = Block::default().title("Projects").borders(Borders::ALL);
-        frame.render_widget(header, chunks[0]);
+        let chunks = Layout::horizontal([
+            Constraint::Min(1), // projects list
+            Constraint::Min(1), // task list
+            Constraint::Min(1), // start/stop timer question
+        ]).split(area);
+
 
         // project list
-        self.projects_list_widget.render(frame, chunks[1]);
+        self.projects_list_widget.render(frame, chunks[0]);
 
+        // task list
+        self.task_list_widget.render(frame, chunks[1]);
+
+        // dialog boxes
         if let Some(text_edit_dialog) = &mut self.text_edit_dialog {
             text_edit_dialog.render(frame, area);
         }
@@ -65,12 +78,6 @@ impl ProjectOverviewScreen {
         if let Some(confirmation_dialog) = &mut self.confirmation_dialog {
             confirmation_dialog.render(frame, area);
         }
-
-        // Footer
-        let footer = Block::default()
-            .title("Enter something useful here")
-            .borders(Borders::ALL);
-        frame.render_widget(footer, chunks[2]);
     }
 
     pub fn handle_event(&mut self, event: Event) -> Option<AppAction> {
@@ -129,13 +136,11 @@ impl ProjectOverviewScreen {
             return match dialog_result {
                 DialogResult::None => None,
                 DialogResult::Confirmed(text) => {
-                    self.text_edit_dialog = None;
-                    self.mode = Mode::ProjectSelection;
+                    self.enable_project_selection_mode();
                     Some(AppAction::CreateProject(text))
                 }
                 DialogResult::Cancelled => {
-                    self.mode = Mode::ProjectSelection;
-                    self.text_edit_dialog = None;
+                    self.enable_project_selection_mode();
                     None
                 }
             };
@@ -149,20 +154,16 @@ impl ProjectOverviewScreen {
             return match dialog_result {
                 DialogResult::None => None,
                 DialogResult::Confirmed(text) => {
-                    self.text_edit_dialog = None;
                     let project = self.selected_project.clone();
-                    self.selected_project = None;
-                    self.mode = Mode::ProjectSelection;
+
+                    self.enable_project_selection_mode();
                     if let Some(selected_project) = project {
                         return Some(AppAction::RenameProject(selected_project.id, text));
                     };
-                    self.selected_project = None;
                     panic!("Try to edit project, but no object is selected!")
                 }
                 DialogResult::Cancelled => {
-                    self.text_edit_dialog = None;
-                    self.selected_project = None;
-                    self.mode = Mode::ProjectSelection;
+                    self.enable_project_selection_mode();
                     None
                 }
             };
@@ -176,21 +177,15 @@ impl ProjectOverviewScreen {
             return match result {
                 ConfirmationResult::None => None,
                 ConfirmationResult::Confirmed => {
-                    self.confirmation_dialog = None;
-                    self.mode = Mode::ProjectSelection;
-
                     let project = self.selected_project.clone();
-                    self.selected_project = None;
-
+                    self.enable_project_selection_mode();
                     if let Some(selected_project) = project {
                         return Some(AppAction::DeleteProject(selected_project.id));
                     }
                     None
                 }
                 ConfirmationResult::Cancelled => {
-                    self.confirmation_dialog = None;
-                    self.mode = Mode::ProjectSelection;
-                    self.selected_project = None;
+                    self.enable_project_selection_mode();
                     None
                 }
             };
@@ -200,5 +195,15 @@ impl ProjectOverviewScreen {
 
     fn build_text_dialog(&mut self, content: String, title: &str) {
         self.text_edit_dialog = Some(TextEditDialog::new(title, content, 50));
+    }
+
+    fn enable_project_selection_mode(&mut self) {
+        self.mode = Mode::ProjectSelection;
+        self.selected_project = None;
+        self.confirmation_dialog = None;
+        self.text_edit_dialog = None;
+        self.help_text =
+            "[N]ew project | [E]dit project | [D]elete project | <Space>: Select | <Up/Down>"
+                .to_string();
     }
 }
