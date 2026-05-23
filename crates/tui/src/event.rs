1f6ef5c772e95e61e3684e6143f7e6e2c6331ea0
Sebastian <sebastian.borsch1996@gmail.com>
2026-05-23 23:54:07 +0200

    Tried to fix issues with double key presses on windows

--- crates/tui/src/event.rs
+++ crates/tui/src/event.rs
@@ -1,12 +1,28 @@
 use std::time::Duration;
+use crossterm::event::Event;
+
 pub enum TuiEvent {
     Tick,
     Input(crossterm::event::Event),
 }
 pub fn read_event(timeout: Duration) -> anyhow::Result<TuiEvent> {
-    if crossterm::event::poll(timeout)? {
-        Ok(TuiEvent::Input(crossterm::event::read()?))
-    } else {
-        Ok(TuiEvent::Tick)
+
+    if !crossterm::event::poll(timeout)? {
+        return Ok(TuiEvent::Tick);
+    }
+
+    loop {
+        let event = crossterm::event::read()?;
+        match event{
+            Event::Key(key_event) =>{
+                if key_event.is_press(){
+                    return Ok(TuiEvent::Input(event));
+                }
+
+                // Ignore Release / Repeat events
+                continue
+            },
+            other => return Ok(TuiEvent::Input(other))
+        }
     }
 }
